28a98ede3f130e25ebf8f931c1da1757c7b1ddee
MihaZupan <mihazupan.zupan1@gmail.com>
2020-07-04 18:39:22 +0200

    Try not to crash node app on failure

--- MihuBotMC/app.js
+++ MihuBotMC/app.js
@@ -4,12 +4,7 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 var mc = require('minecraft-protocol');
-var client = mc.createClient({
-  host: "darlings.me",
-  port: 25565,
-  username: "mihazupan.zupan1@gmail.com",
-  password: "***REMOVED***",
-});
+var client = newClient();
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -28,3 +23,21 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 })
+
+process
+  .on('unhandledRejection', (reason, p) => {
+    console.error(reason, 'Unhandled Rejection at Promise', p);
+  })
+  .on('uncaughtException', err => {
+    console.error(err, 'Uncaught Exception thrown');
+    client = newClient();
+  });
+
+function newClient() {
+  return mc.createClient({
+    host: "darlings.me",
+    port: 25565,
+    username: "mihazupan.zupan1@gmail.com",
+    password: "***REMOVED***",
+  });
+}
