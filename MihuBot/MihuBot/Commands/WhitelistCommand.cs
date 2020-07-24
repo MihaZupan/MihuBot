1536c01d873572259aaf8bdd882c44496bd8e0c8
MihaZupan <mihazupan.zupan1@gmail.com>
2020-07-24 19:53:01 +0200

    Add whitelist remove

--- MihuBot/MihuBot/Commands/WhitelistCommand.cs
+++ MihuBot/MihuBot/Commands/WhitelistCommand.cs
@@ -26,24 +26,43 @@ namespace MihuBot.Commands
 
                 string args = ctx.ArgumentString;
 
-                if (ctx.IsFromAdmin && args.Equals("list", StringComparison.OrdinalIgnoreCase))
+                if (ctx.IsFromAdmin)
                 {
-                    Memory<string> entryList = entries.Select(pair => FormatLine(pair.Key, pair.Value)).ToArray();
-
-                    while (!entryList.IsEmpty)
+                    if (args.Equals("list", StringComparison.OrdinalIgnoreCase))
                     {
-                        var part = entryList.Slice(0, Math.Min(entryList.Length, 30));
-                        entryList = entryList.Slice(part.Length);
+                        Memory<string> entryList = entries.Select(pair => FormatLine(pair.Key, pair.Value)).ToArray();
 
-                        await ctx.ReplyAsync($"```\n{string.Join('\n', part.ToArray())}\n```");
+                        while (!entryList.IsEmpty)
+                        {
+                            var part = entryList.Slice(0, Math.Min(entryList.Length, 30));
+                            entryList = entryList.Slice(part.Length);
+
+                            await ctx.ReplyAsync($"```\n{string.Join('\n', part.ToArray())}\n```");
+                        }
+                        return;
+
+                        string FormatLine(ulong userId, string username)
+                        {
+                            string discordUsername = ctx.Discord.GetUser(userId).Username;
+
+                            return username.PadRight(17, ' ') + discordUsername.Substring(0, Math.Min(discordUsername.Length, 20));
+                        }
                     }
-                    return;
-
-                    string FormatLine(ulong userId, string username)
+                    else if (ctx.Arguments.Length > 1 && ctx.Arguments[0].Equals("remove", StringComparison.OrdinalIgnoreCase))
                     {
-                        string discordUsername = ctx.Discord.GetUser(userId).Username;
+                        if (ulong.TryParse(ctx.Arguments[1], out ulong id) && entries.TryGetValue(id, out string username))
+                        {
+                            entries.Remove(id);
+                            await McCommand.RunMinecraftCommandAsync("whitelist remove " + username);
+                            await McCommand.RunMinecraftCommandAsync("kick " + username);
 
-                        return username.PadRight(17, ' ') + discordUsername.Substring(0, Math.Min(discordUsername.Length, 20));
+                            await ctx.ReplyAsync($"Removed {ctx.Discord.GetUser(id).Username} ({username}) from the whitelist", mention: true);
+                        }
+                        else
+                        {
+                            await ctx.ReplyAsync("Can't find a matching whitelist entry", mention: true);
+                        }
+                        return;
                     }
                 }
 
