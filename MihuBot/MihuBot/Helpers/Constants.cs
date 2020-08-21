54b1215fb0abfe22deeb03bc6c6c9ec501a129bd
Miha Zupan <mihazupan.zupan1@gmail.com>
2020-08-22 01:17:10 +0200

    Update admins list

--- MihuBot/MihuBot/Helpers/Constants.cs
+++ MihuBot/MihuBot/Helpers/Constants.cs
@@ -15,8 +15,6 @@ namespace MihuBot.Helpers
             KnownUsers.Jaeger,
             KnownUsers.Jordan,
             KnownUsers.Doomi,
-            KnownUsers.Liv,
-            KnownUsers.DreisBlue,
         };
 
         public static readonly Dictionary<ulong, HashSet<ulong>> GuildMods = new Dictionary<ulong, HashSet<ulong>>
@@ -31,7 +29,7 @@ namespace MihuBot.Helpers
             },
             {
                 Guilds.LiverGang,
-                new HashSet<ulong> { KnownUsers.Angelo, }
+                new HashSet<ulong> { KnownUsers.Angelo, KnownUsers.Liv }
             },
             {
                 Guilds.Arisas,
@@ -80,9 +78,8 @@ namespace MihuBot.Helpers
         public const ulong Jaeger       = 145719024544645120ul;
         public const ulong Jordan       = 236455327535464458ul;
         public const ulong Doomi        = 238754130233917440ul;
-        public const ulong Liv          = 244637014547234816ul;
-        public const ulong DreisBlue    = 134162712191172608ul;
 
+        public const ulong Liv          = 244637014547234816ul;
         public const ulong CurtIs       = 237788815626862593ul;
         public const ulong Christian    = 397254656025427968ul;
         public const ulong Arisa        = 417794401721384960ul;
