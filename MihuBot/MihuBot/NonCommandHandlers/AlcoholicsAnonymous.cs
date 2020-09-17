8ef2ef59517bab5cdb255479c495f6b30a1b92aa
MihaZupan <mihazupan.zupan1@gmail.com>
2020-09-17 16:29:08 +0200

    Add @alcoholics

---
+++ MihuBot/MihuBot/NonCommandHandlers/AlcoholicsAnonymous.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using MihuBot.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MihuBot.NonCommandHandlers
+{
+    public sealed class AlcoholicsAnonymous : NonCommandHandler
+    {
+        private static readonly HashSet<ulong> Alcoholics = new HashSet<ulong>()
+        {
+            KnownUsers.Miha,
+            KnownUsers.Jordan,
+            KnownUsers.James,
+            KnownUsers.Christian,
+            KnownUsers.PaulK,
+            KnownUsers.Sticky
+        };
+
+        public override Task HandleAsync(MessageContext ctx)
+        {
+            if ((ctx.IsFromAdmin || Alcoholics.Contains(ctx.AuthorId)) &&
+                ctx.Content.StartsWith("@alcoholics", StringComparison.OrdinalIgnoreCase))
+            {
+                return HandleAsyncCore();
+            }
+
+            return Task.CompletedTask;
+
+            async Task HandleAsyncCore()
+            {
+                var alcoholics = Alcoholics.Where(a => a != ctx.AuthorId);
+                await ctx.ReplyAsync(string.Join(' ', alcoholics.Select(a => MentionUtils.MentionUser(a))));
+            }
+        }
+    }
+}
