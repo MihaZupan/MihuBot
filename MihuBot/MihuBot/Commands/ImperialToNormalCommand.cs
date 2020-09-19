4128f8eda0fd5c8af2839163409754898ca5e6e3
MihaZupan <mihazupan.zupan1@gmail.com>
2020-09-19 23:54:51 +0200

    Add imperial to normal command

---
+++ MihuBot/MihuBot/Commands/ImperialToNormalCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MihuBot.Commands
+{
+    public sealed class ImperialToNormalCommand : CommandBase
+    {
+        public override string Command => "imperialtonormal";
+
+        public override async Task ExecuteAsync(CommandContext ctx)
+        {
+            var fahrenheit = Regex.Match(ctx.Content, @"^(\d+?) ?f$", RegexOptions.IgnoreCase);
+            if (fahrenheit.Success && double.TryParse(fahrenheit.Groups[1].Value, out double value))
+            {
+                double celsius = (value - 32) / 1.8d;
+                await ctx.ReplyAsync($"{(int)celsius} °C");
+            }
+        }
+    }
+}
