c05e89b65c2e56733c668570f789ee6a1965502f
MihaZupan <mihazupan.zupan1@gmail.com>
2020-09-27 11:54:59 +0200

    Add initial Speech recognition test

---
+++ MihuBot/MihuBot/Commands/JoinCommand.cs
@@ -0,0 +1,26 @@
+﻿using MihuBot.Helpers;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MihuBot.Commands
+{
+    public sealed class JoinCommand : CommandBase
+    {
+        public override string Command => "join";
+
+        public override async Task ExecuteAsync(CommandContext ctx)
+        {
+            if (!ctx.IsFromAdmin)
+                return;
+
+            var vc = ctx.Guild.VoiceChannels.FirstOrDefault(vc => vc.Users.Any(u => u.Id == ctx.AuthorId));
+            if (vc is null)
+            {
+                await ctx.ReplyAsync("Join a VC first");
+                return;
+            }
+
+            await AudioClient.TryGetOrJoinAsync(ctx.Guild, vc);
+        }
+    }
+}
--- MihuBot/MihuBot/Helpers/AudioClient.cs
+++ MihuBot/MihuBot/Helpers/AudioClient.cs
@@ -1,315 +1,115 @@
-﻿using Discord;
-using Discord.Audio;
+﻿using Discord.Audio;
 using Discord.WebSocket;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace MihuBot.Helpers
 {
     public class AudioClient
     {
-        private static readonly Dictionary<ulong, AudioClient> _audioClients = new Dictionary<ulong, AudioClient>();
-
-
-        private static readonly HttpClient HttpClient = new HttpClient();
-
-        private readonly SocketGuild _guild;
-        private readonly AudioOutStream _audioStream;
-
-        private readonly Queue<AudioSource> _sourcesQueue;
-        private AudioSource _activeStream;
-        private IVoiceChannel _voiceChannel;
-
-        private AudioClient(SocketGuild guild, IVoiceChannel voiceChannel)
-        {
-            _guild = guild;
-            _voiceChannel = voiceChannel;
-            _audioStream = guild.AudioClient.CreatePCMStream(AudioApplication.Music, voiceChannel.Bitrate, packetLoss: 1);
-            _sourcesQueue = new Queue<AudioSource>();
-        }
+        private static readonly Dictionary<ulong, Task<AudioClient>> _audioClients = new Dictionary<ulong, Task<AudioClient>>();
 
         public static async Task<AudioClient> TryGetOrJoinAsync(SocketGuild guild, SocketVoiceChannel channelToJoin)
         {
-            lock (_audioClients)
-            {
-                if (_audioClients.TryGetValue(guild.Id, out var audioClient))
-                {
-                    return audioClient;
-                }
-            }
-
-            if (channelToJoin is null)
-                return null;
-
-            await channelToJoin.ConnectAsync();
+            TaskCompletionSource<AudioClient> tcs = null;
+            Task<AudioClient> audioClientTask;
 
             lock (_audioClients)
             {
-                if (_audioClients.TryGetValue(guild.Id, out var audioClient))
+                if (!_audioClients.TryGetValue(guild.Id, out audioClientTask))
                 {
-                    return audioClient;
-                }
+                    tcs = new TaskCompletionSource<AudioClient>(TaskCreationOptions.RunContinuationsAsynchronously);
+                    audioClientTask = tcs.Task;
 
-                audioClient = new AudioClient(guild, channelToJoin);
-                _audioClients.Add(guild.Id, audioClient);
-                return audioClient;
-            }
-        }
-
-        private static readonly string[] ValidExtensions = new string[]
-        {
-            ".mp3",
-            ".wav",
-            ".opus",
-            ".flac"
-        };
-
-        public async Task TryQueueContentAsync(SocketMessage message)
-        {
-            Uri uri = null;
-            string url = message.Content
-                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                .FirstOrDefault(p => p.Contains("://") && Uri.TryCreate(p, UriKind.Absolute, out uri));
-
-            if (url is null)
-            {
-                await message.ReplyAsync("Please supply a valid url", mention: true);
-                return;
-            }
-
-            AudioSource source = null;
-
-            if (YoutubeHelper.TryParseVideoId(url, out string videoId))
-            {
-                source = new YoutubeSource(videoId);
-            }
-            else
-            {
-                string extension = Path.GetExtension(uri.AbsolutePath);
-
-                if (ValidExtensions.Contains(extension, StringComparison.OrdinalIgnoreCase))
-                {
-                    source = new DirectAudioUrlSource(uri);
-                }
-                else
-                {
-                    await message.ReplyAsync($"Source must be: {string.Join(", ", ValidExtensions.Select(ext => "`" + ext + "`"))}");
+                    _audioClients.Add(guild.Id, audioClientTask);
                 }
             }
 
-            if (source is null)
-                return;
-
-            try
+            if (tcs != null)
             {
-                string error = await source.PreInitAsync();
-
-                if (error != null)
-                {
-                    await message.ReplyAsync(error, mention: true);
-                    return;
-                }
-            }
-            catch (Exception ex)
-            {
-                await Program.DebugAsync(ex.ToString());
-                return;
-            }
-
-            AddAudioSource(source);
-        }
-
-        private void AddAudioSource(AudioSource source)
-        {
-            lock (_audioStream)
-            {
-                if (_activeStream is null)
-                {
-                    _activeStream = source;
-                    _ = Task.Run(async () => await SourceRelayTaskAsync());
-                }
-                else
-                {
-                    _sourcesQueue.Enqueue(source);
-                }
-            }
-        }
-
-        private async Task SourceRelayTaskAsync()
-        {
-            byte[] buffer = new byte[32 * 1024];
-
-            while (true)
-            {
-                Debug.Assert(_activeStream != null);
-
                 try
                 {
-                    await _activeStream.InitAsync();
-
-                    int read;
-                    while ((read = await _activeStream.ReadAsync(buffer)) > 0)
+                    await channelToJoin.ConnectAsync(selfDeaf: false);
+                    tcs.SetResult(new AudioClient(guild));
+                }
+                catch (Exception ex)
+                {
+                    lock (_audioClients)
                     {
-                        await _audioStream.WriteAsync(buffer.AsMemory(0, read));
+                        _audioClients.Remove(guild.Id);
+                    }
+
+                    tcs.SetException(ex);
+                }
+            }
+
+            return await audioClientTask;
+        }
+
+        private readonly SocketGuild _guild;
+
+        private AudioClient(SocketGuild guild)
+        {
+            _guild = guild;
+            _guild.AudioClient.StreamCreated += AudioClient_StreamCreatedAsync;
+            _guild.AudioClient.Disconnected += AudioClient_DisconnectedAsync;
+        }
+
+        private Task AudioClient_DisconnectedAsync(Exception arg)
+        {
+            lock (_audioClients)
+            {
+                _audioClients.Remove(_guild.Id);
+            }
+            return Task.CompletedTask;
+        }
+
+        private Task AudioClient_StreamCreatedAsync(ulong id, AudioInStream stream)
+        {
+            _ = Task.Run(async () =>
+            {
+                try
+                {
+                    var config = SpeechConfig.FromSubscription(Secrets.AzureSpeech.SubscriptionKey, Secrets.AzureSpeech.Region);
+
+                    using var pushStream = AudioInputStream.CreatePushStream(AudioStreamFormat.GetCompressedFormat(AudioStreamContainerFormat.OGG_OPUS));
+                    using var audioInput = AudioConfig.FromStreamInput(pushStream);
+                    using var recognizer = new SpeechRecognizer(config, audioInput);
+
+                    recognizer.Recognized += (s, e) =>
+                    {
+                        if (e.Result.Reason == ResultReason.RecognizedSpeech)
+                        {
+                            Console.WriteLine($"RECOGNIZED: Text={e.Result.Text}");
+                        }
+                    };
+
+                    await recognizer.StartContinuousRecognitionAsync();
+                    try
+                    {
+                        byte[] buffer = new byte[4096];
+
+                        int read;
+                        while ((read = await stream.ReadAsync(buffer)) > 0)
+                        {
+                            pushStream.Write(buffer, read);
+                        }
+                    }
+                    finally
+                    {
+                        await recognizer.StopContinuousRecognitionAsync();
                     }
                 }
                 catch (Exception ex)
                 {
-                    await Program.DebugAsync(ex.ToString());
+                    await Logger.Instance.DebugAsync(ex.ToString());
                 }
+            });
 
-                try
-                {
-                    await _activeStream.CleanupAsync();
-                }
-                catch { }
-
-                lock (_sourcesQueue)
-                {
-                    if (_sourcesQueue.Count == 0)
-                    {
-                        _activeStream = null;
-                        break;
-                    }
-
-                    _activeStream = _sourcesQueue.Dequeue();
-                    continue;
-                }
-            }
-        }
-
-
-        public static Stream ReadAsPCM(string sourcePath)
-        {
-            Console.WriteLine($"Reading {sourcePath} as PCM");
-
-            using Process ffmpeg = new Process();
-            ffmpeg.StartInfo.FileName = @"ffmpeg";
-            ffmpeg.StartInfo.Arguments = $"-y -hide_banner -loglevel warning -i \"{sourcePath}\" -filter:a \"volume={Constants.VCDefaultVolume}\" -ac 2 -f s16le -acodec pcm_s16le -vn -";
-            ffmpeg.StartInfo.UseShellExecute = false;
-            ffmpeg.StartInfo.RedirectStandardOutput = true;
-            ffmpeg.Start();
-
-            return ffmpeg.StandardOutput.BaseStream;
-        }
-
-        public sealed class DirectAudioUrlSource : AudioSource
-        {
-            private readonly Uri _uri;
-
-            private string _tempFilePath;
-            private Stream _ffmpegPcmStream;
-
-            public DirectAudioUrlSource(Uri uri)
-            {
-                _uri = uri;
-            }
-
-            public override ValueTask<int> ReadAsync(Memory<byte> buffer) => _ffmpegPcmStream.ReadAsync(buffer);
-
-            public override async Task InitAsync()
-            {
-                _tempFilePath = Path.Combine(Path.GetTempPath(), Guid.NewGuid() + Path.GetExtension(_uri.AbsolutePath).ToLowerInvariant());
-
-                var response = await HttpClient.GetAsync(_uri, HttpCompletionOption.ResponseHeadersRead);
-                var stream = await response.Content.ReadAsStreamAsync();
-
-                using var fs = File.OpenWrite(_tempFilePath);
-                await stream.CopyToAsync(fs);
-                await fs.FlushAsync();
-
-                if (!File.Exists(_tempFilePath))
-                    throw new FileNotFoundException(null, _tempFilePath);
-
-                if (new FileInfo(_tempFilePath).Length < 4097)
-                    throw new Exception("Too short");
-
-                _ffmpegPcmStream = ReadAsPCM(_tempFilePath);
-            }
-
-            public override Task CleanupAsync()
-            {
-                try
-                {
-                    _ffmpegPcmStream?.Dispose();
-                }
-                catch { }
-
-                File.Delete(_tempFilePath);
-                return Task.CompletedTask;
-            }
-        }
-
-        public sealed class YoutubeSource : AudioSource
-        {
-            private readonly string _id;
-
-            private string _tempFilePath;
-            private Stream _ffmpegPcmStream;
-
-            public YoutubeSource(string id)
-            {
-                _id = id;
-            }
-
-            public override async Task<string> PreInitAsync()
-            {
-                try
-                {
-                    var video = await YoutubeHelper.Youtube.Videos.GetAsync(_id);
-
-                    if (video.Duration > TimeSpan.FromHours(2))
-                    {
-                        return "Too long";
-                    }
-                }
-                catch (Exception ex)
-                {
-                    await Program.DebugAsync(ex.ToString());
-                    return "Something went wrong";
-                }
-
-                return null;
-            }
-
-            public override async Task InitAsync()
-            {
-                var bestAudio = YoutubeHelper.GetBestAudio(await YoutubeHelper.Youtube.Videos.Streams.GetManifestAsync(_id), out string extension);
-
-                _tempFilePath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + extension);
-                await YoutubeHelper.Youtube.Videos.Streams.DownloadAsync(bestAudio, _tempFilePath);
-
-                _ffmpegPcmStream = ReadAsPCM(_tempFilePath);
-            }
-
-            public override ValueTask<int> ReadAsync(Memory<byte> buffer) => _ffmpegPcmStream.ReadAsync(buffer);
-
-            public override Task CleanupAsync()
-            {
-                try
-                {
-                    _ffmpegPcmStream?.Dispose();
-                }
-                catch { }
-
-                if (_tempFilePath != null)
-                    File.Delete(_tempFilePath);
-
-                return Task.CompletedTask;
-            }
-        }
-
-        public abstract class AudioSource
-        {
-            public virtual Task<string> PreInitAsync() => Task.FromResult<string>(null);
-            public abstract Task InitAsync();
-            public abstract ValueTask<int> ReadAsync(Memory<byte> buffer);
-            public virtual Task CleanupAsync() => Task.CompletedTask;
+            return Task.CompletedTask;
         }
     }
 }
