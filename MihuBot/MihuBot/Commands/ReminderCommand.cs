4f852d1a9fb8687de55ad00a435470a047663a3f
MihaZupan <mihazupan.zupan1@gmail.com>
2020-10-13 22:19:18 +0200

    Improve Reminder time parsing

--- MihuBot/MihuBot/Commands/ReminderCommand.cs
+++ MihuBot/MihuBot/Commands/ReminderCommand.cs
@@ -3,6 +3,7 @@ using Discord.WebSocket;
 using MihuBot.Helpers;
 using MihuBot.Reminders;
 using System;
+using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -24,12 +25,17 @@ namespace MihuBot.Commands
             TimeSpan.FromSeconds(2));
 
         private static readonly Regex _timeRegex = new Regex(
-            @"(\d+|a|an)? ?(s|sec|seconds?|m|min|minutes?|hr?s?|hours?|d|days?|w|weeks?|months?|y|years?)(?: |$)",
+            @"(?:and )?(\d*?(?:[\.,]\d+)?|a|an)? ?(s|sec|seconds?|m|min|minutes?|hr?s?|hours?|d|days?|w|weeks?|months?|y|years?)(?:[ ,]|$)",
             RegexOptions.IgnoreCase | RegexOptions.Compiled,
             TimeSpan.FromSeconds(5));
 
         private static bool TryParseRemindTime(string time, out DateTime dateTime)
         {
+            dateTime = default;
+
+            if (string.IsNullOrEmpty(time))
+                return false;
+
             bool at = char.ToLowerInvariant(time[0]) == 'a';
             time = time.Trim();
 
@@ -39,7 +45,6 @@ namespace MihuBot.Commands
             }
             else
             {
-                dateTime = default;
                 var matches = _timeRegex.Matches(time);
 
                 if (matches.Count > 10)
@@ -50,42 +55,50 @@ namespace MihuBot.Commands
 
                 foreach (Match m in matches)
                 {
-                    int number = 1;
+                    double number = 1;
                     if (m.Groups[1].Success)
                     {
                         string quantifier = m.Groups[1].Value;
                         if (char.ToLowerInvariant(quantifier[0]) != 'a')
                         {
-                            if (!int.TryParse(quantifier, out number))
+                            quantifier = quantifier.Replace(',', '.');
+                            if (!double.TryParse(quantifier, NumberStyles.Any, CultureInfo.InvariantCulture, out number))
                                 return false;
                         }
                     }
 
+                    if (number > int.MaxValue)
+                        return false;
+
                     string type = m.Groups[2].Value.ToLowerInvariant();
 
-                    if (number > 1_000_000 && type[0] != 's')
-                        return false;
-
-                    TimeSpan segment = type[0] switch
+                    try
                     {
-                        's' => TimeSpan.FromSeconds(number),
-                        'm' when type.StartsWith("month") => now.AddMonths(number) - now,
-                        'm' => TimeSpan.FromMinutes(number),
-                        'h' => TimeSpan.FromHours(number),
-                        'd' => TimeSpan.FromDays(number),
-                        'w' => TimeSpan.FromDays(number * 7),
-                        'y' => now.AddYears(number) - now,
-                        _ => TimeSpan.Zero
-                    };
+                        dateTime += type[0] switch
+                        {
+                            's' => TimeSpan.FromSeconds(number),
+                            'm' when type.StartsWith("month") => FromMonths(dateTime, number),
+                            'm' => TimeSpan.FromMinutes(number),
+                            'h' => TimeSpan.FromHours(number),
+                            'd' => TimeSpan.FromDays(number),
+                            'w' => TimeSpan.FromDays(number * 7),
+                            'y' => FromYears(dateTime, number),
+                            _ => TimeSpan.Zero
+                        };
 
-                    if (segment == TimeSpan.Zero)
-                        return false;
+                        static TimeSpan FromMonths(DateTime now, double number) =>
+                            now.AddMonths((int)number) - now + TimeSpan.FromDays((365.25d / 12d) * (number % 1));
 
-                    dateTime += segment;
+                        static TimeSpan FromYears(DateTime now, double number) =>
+                            now.AddYears((int)number) - now + TimeSpan.FromDays(365.25d * (number % 1));
+                    }
+                    catch { return false; }
                 }
 
-                if (dateTime != now)
-                    return true;
+                if (dateTime == now || dateTime.Year > 12_000)
+                    return false;
+
+                return true;
             }
 
             dateTime = default;
