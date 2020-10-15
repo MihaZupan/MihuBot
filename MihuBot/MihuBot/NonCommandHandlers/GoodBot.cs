a8081901a577d62bd214b13708ac91984e85920f
MihaZupan <mihazupan.zupan1@gmail.com>
2020-10-15 07:57:48 +0200

    Add cooldown to good bot handler

--- MihuBot/MihuBot/NonCommandHandlers/GoodBot.cs
+++ MihuBot/MihuBot/NonCommandHandlers/GoodBot.cs
@@ -6,9 +6,12 @@ namespace MihuBot.NonCommandHandlers
 {
     public sealed class GoodBot : NonCommandHandler
     {
+        protected override TimeSpan Cooldown => TimeSpan.FromMinutes(1);
+        protected override int CooldownToleranceCount => 30;
+
         public override Task HandleAsync(MessageContext ctx)
         {
-            if (ctx.Content.Equals("good bot", StringComparison.OrdinalIgnoreCase))
+            if (ctx.Content.Equals("good bot", StringComparison.OrdinalIgnoreCase) && TryEnter(ctx))
                 return HandleAsyncCore();
 
             return Task.CompletedTask;
