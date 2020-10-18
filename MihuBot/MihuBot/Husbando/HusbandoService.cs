dde76ecad99a913ceb597184b7217315e386c835
MihaZupan <mihazupan.zupan1@gmail.com>
2020-10-18 20:06:20 +0200

    Husbando => Husbandos

--- MihuBot/MihuBot/Husbando/HusbandoService.cs
+++ MihuBot/MihuBot/Husbando/HusbandoService.cs
@@ -1,5 +1,4 @@
 ﻿using MihuBot.Helpers;
-using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,55 +10,6 @@ namespace MihuBot.Husbando
     {
         private readonly SynchronizedLocalJsonStore<Dictionary<ulong, (List<ulong> Husbandos, List<ulong> Waifus)>> _husbandos =
             new SynchronizedLocalJsonStore<Dictionary<ulong, (List<ulong> Husbandos, List<ulong> Waifus)>>("Husbandos.json");
-
-        private readonly IConnectionMultiplexer _redis;
-
-        public HusbandoService(IConnectionMultiplexer redis)
-        {
-            _redis = redis;
-
-            if (_husbandos.QueryAsync(i => i).Result.Count == 0)
-            {
-                Task.Run(async () =>
-                {
-                    var server = _redis.GetServer(Secrets.Redis.DatabaseAddress, 6379);
-
-                    var keys = new List<(RedisKey Key, ulong Id, bool Husbando)>();
-
-                    await foreach (var husbandoKey in server.KeysAsync(pattern: "husbando-*"))
-                        keys.Add((husbandoKey, ulong.Parse(husbandoKey.ToString().Split('-')[1]), true));
-
-                    await foreach (var waifuKey in server.KeysAsync(pattern: "waifu-*"))
-                        keys.Add((waifuKey, ulong.Parse(waifuKey.ToString().Split('-')[1]), false));
-
-                    var husbandos = await _husbandos.EnterAsync();
-                    try
-                    {
-                        var db = _redis.GetDatabase();
-                        foreach (var key in keys)
-                        {
-                            if (!husbandos.TryGetValue(key.Id, out (List<ulong> Husbandos, List<ulong> Waifus) matches))
-                            {
-                                matches = husbandos[key.Id] = (new List<ulong>(), new List<ulong>());
-                            }
-
-                            var newMatches = await db.SetScanAsync(key.Key).ToArrayAsync();
-
-                            (key.Husbando ? matches.Husbandos : matches.Waifus)
-                                .AddRange(newMatches.Select(m => ulong.Parse(m)));
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex);
-                    }
-                    finally
-                    {
-                        _husbandos.Exit();
-                    }
-                });
-            }
-        }
 
         public async ValueTask<ulong?> TryGetRandomMatchAsync(bool husbando, ulong user)
         {
