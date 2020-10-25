e44c7ab41d1b27f99a4e1b641c05667fcfb5f00d
MihaZupan <mihazupan.zupan1@gmail.com>
2020-10-25 04:42:54 +0100

    Improve ImperialToNormal

--- MihuBot/MihuBot/Commands/ImperialToNormalCommand.cs
+++ MihuBot/MihuBot/Commands/ImperialToNormalCommand.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MihuBot.Commands
@@ -7,14 +9,95 @@ namespace MihuBot.Commands
     {
         public override string Command => "imperialtonormal";
 
+        public override string[] Aliases => new[] { "normaltoimperial" };
+
         public override async Task ExecuteAsync(CommandContext ctx)
         {
-            var fahrenheit = Regex.Match(ctx.ArgumentString, @"^(\d+?) ?f$", RegexOptions.IgnoreCase);
-            if (fahrenheit.Success && double.TryParse(fahrenheit.Groups[1].Value, out double value))
+            bool imperialToNormal = ctx.Command == "imperialtonormal";
+
+            string[] parts = ctx.ArgumentString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length == 1)
+                return;
+
+            var quantifierMatch = Regex.Match(ctx.ArgumentString, @"^-?[\d\.,]*");
+            if (!quantifierMatch.Success)
+                return;
+
+            decimal value = 1;
+            if (quantifierMatch.Length != 0)
             {
-                double celsius = (value - 32) / 1.8d;
-                await ctx.ReplyAsync($"{(int)celsius} °C");
+                var quantifier = quantifierMatch.Value.Replace(',', '.');
+                if (!decimal.TryParse(quantifier, NumberStyles.Any, CultureInfo.InvariantCulture, out value))
+                    return;
             }
+
+            string type = ctx.ArgumentString.Substring(quantifierMatch.Length).Trim().ToLowerInvariant();
+            if (type.Length == 0)
+                return;
+
+            Func<decimal, decimal> conversion = null;
+            string format = null;
+            bool appendS = false;
+
+            if (imperialToNormal)
+            {
+                switch (type)
+                {
+                    case "f":
+                        if (value > 0 && value < 10) goto case "feet";
+                        else goto case "fahrenheit";
+
+                    case "fahrenheit":
+                        conversion = v => (v - 32) / 1.8m;
+                        format = "°C";
+                        break;
+
+                    case "ft": case "feet":
+                        conversion = v => v * 0.3048m;
+                        format = "meter";
+                        appendS = true;
+                        break;
+
+                    case "i": case "inch": case "inchs": case "inches":
+                        conversion = v => v * 0.0254m;
+                        format = "meter";
+                        appendS = true;
+                        break;
+
+                    case "gln": case "glns": case "gallon": case "gallons":
+                        conversion = v => v * 3.785411784m;
+                        format = "liter";
+                        appendS = true;
+                        break;
+                }
+            }
+            else
+            {
+                switch (type)
+                {
+                    case "m": case "ms": case "mtr": case "mtrs": case "meter": case "meters":
+                        conversion = v => v * 117.64705882353m;
+                        format = "barleycorn";
+                        appendS = true;
+                        break;
+
+                    case "l": case "ltr": case "ltrs": case "liter": case "liters":
+                        conversion = v => v * 0.2641720524m;
+                        format = "gallon";
+                        appendS = true;
+                        break;
+                }
+            }
+
+            if (conversion is null)
+                return;
+
+            value = conversion(value);
+
+            if (appendS && value % 1 != 0)
+                format += 's';
+
+            await ctx.ReplyAsync($"{decimal.Round(value, 2):N2} {format}");
         }
     }
 }
