9aec462637a04911d7f77f1c2d04863279a7e83b
MihaZupan <mihazupan.zupan1@gmail.com>
2020-11-06 07:33:56 +0100

    Fix ConfigurationService

--- MihuBot/MihuBot/Configuration/ConfigurationService.cs
+++ MihuBot/MihuBot/Configuration/ConfigurationService.cs
@@ -7,16 +7,18 @@ namespace MihuBot.Configuration
 {
     public sealed class ConfigurationService : IConfigurationService
     {
-        private readonly Dictionary<ulong?, Dictionary<string, string>> _configuration;
-        private readonly SynchronizedLocalJsonStore<Dictionary<ulong?, Dictionary<string, string>>> _store;
+        private readonly Dictionary<ulong, Dictionary<string, string>> _configuration;
+        private readonly Dictionary<string, string> _globalConfiguration;
+        private readonly SynchronizedLocalJsonStore<Dictionary<ulong, Dictionary<string, string>>> _store;
+        private readonly SynchronizedLocalJsonStore<Dictionary<string, string>> _globalStore;
         private readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
 
         public ConfigurationService()
         {
-            _store = new SynchronizedLocalJsonStore<Dictionary<ulong?, Dictionary<string, string>>>("Configuration.json",
+            _store = new SynchronizedLocalJsonStore<Dictionary<ulong, Dictionary<string, string>>>("Configuration.json",
                 dictionary =>
                 {
-                    var configuration = new Dictionary<ulong?, Dictionary<string, string>>();
+                    var configuration = new Dictionary<ulong, Dictionary<string, string>>();
                     foreach (var entry in dictionary)
                     {
                         configuration.Add(entry.Key, new Dictionary<string, string>(entry.Value, StringComparer.OrdinalIgnoreCase));
@@ -24,7 +26,11 @@ namespace MihuBot.Configuration
                     return configuration;
                 });
 
+            _globalStore = new SynchronizedLocalJsonStore<Dictionary<string, string>>("GlobalConfiguration.json",
+                dictionary => new Dictionary<string, string>(dictionary, StringComparer.OrdinalIgnoreCase));
+
             _configuration = _store.DangerousGetValue();
+            _globalConfiguration = _globalStore.DangerousGetValue();
         }
 
         public void Set(ulong? context, string key, string value)
@@ -34,8 +40,16 @@ namespace MihuBot.Configuration
             {
                 _lock.EnterWriteLock();
 
-                if (!_configuration.TryGetValue(context, out Dictionary<string, string> configuration))
-                    configuration = _configuration[context] = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+                Dictionary<string, string> configuration;
+                if (context.HasValue)
+                {
+                    if (!_configuration.TryGetValue(context.Value, out configuration))
+                        configuration = _configuration[context.Value] = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+                }
+                else
+                {
+                    configuration = _globalConfiguration;
+                }
 
                 configuration[key] = value;
             }
@@ -53,15 +67,23 @@ namespace MihuBot.Configuration
             {
                 _lock.EnterWriteLock();
 
-                if (!_configuration.TryGetValue(context, out Dictionary<string, string> configuration))
-                    return false;
+                Dictionary<string, string> configuration;
+                if (context.HasValue)
+                {
+                    if (!_configuration.TryGetValue(context.Value, out configuration))
+                        return false;
+                }
+                else
+                {
+                    configuration = _globalConfiguration;
+                }
 
                 if (!configuration.Remove(key))
                     return false;
 
-                if (configuration.Count == 0)
+                if (configuration.Count == 0 && context.HasValue)
                 {
-                    _configuration.Remove(context);
+                    _configuration.Remove(context.Value);
                 }
 
                 return true;
@@ -78,9 +100,21 @@ namespace MihuBot.Configuration
             _lock.EnterReadLock();
             try
             {
-                value = null;
-                return _configuration.TryGetValue(context, out Dictionary<string, string> configuration)
-                    && configuration.TryGetValue(key, out value);
+                Dictionary<string, string> configuration;
+                if (context.HasValue)
+                {
+                    if (!_configuration.TryGetValue(context.Value, out configuration))
+                    {
+                        value = null;
+                        return false;
+                    }
+                }
+                else
+                {
+                    configuration = _globalConfiguration;
+                }
+
+                return configuration.TryGetValue(key, out value);
             }
             finally
             {
