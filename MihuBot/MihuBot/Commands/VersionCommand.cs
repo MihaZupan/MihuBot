8c44c8703f639016d73a865545e3cf5bbc2e050b
MihaZupan <mihazupan.zupan1@gmail.com>
2020-11-11 20:42:36 +0100

    Add Version command

---
+++ MihuBot/MihuBot/Commands/VersionCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace MihuBot.Commands
+{
+    public sealed class VersionCommand : CommandBase
+    {
+        public override string Command => "version";
+
+        public override async Task ExecuteAsync(CommandContext ctx)
+        {
+            string os = $"{RuntimeInformation.OSDescription} ({RuntimeInformation.OSArchitecture})";
+            string rid = RuntimeInformation.RuntimeIdentifier;
+            await ctx.ReplyAsync($"{RuntimeInformation.FrameworkDescription} on {os} - `{rid}`");
+        }
+    }
+}
