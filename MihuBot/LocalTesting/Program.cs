5b753f314cf5696af6eb802f0d42227f4e127064
Miha Zupan <mihazupan.zupan1@gmail.com>
2021-09-13 20:31:12 +0200

    Add LocalTesting project

---
+++ MihuBot/LocalTesting/Program.cs
@@ -0,0 +1,53 @@
+﻿string token = await File.ReadAllTextAsync(@"C:\MihaZupan\MihuBot\MihuBot\LocalTesting\BotToken.txt");
+using var client = new InitializedDiscordClient(new DiscordSocketConfig { }, TokenType.Bot, token);
+await client.EnsureInitializedAsync();
+
+client.InteractionCreated += async interaction =>
+{
+    switch (interaction.Type)
+    {
+        case InteractionType.MessageComponent:
+            if (interaction is SocketMessageComponent messageComponent)
+            {
+                SocketMessageComponentData data = messageComponent.Data;
+
+                if (data.Type == ComponentType.Button && data.CustomId.StartsWith("reminder-", StringComparison.Ordinal))
+                {
+                    await messageComponent.Message.ModifyAsync(m => m.Components = null);
+
+                    switch (data.CustomId)
+                    {
+                        case "reminder-snooze-1h":
+                            await messageComponent.FollowupAsync("You will be reminded again in 1 hour", ephemeral: true);
+                            break;
+                    }
+                }
+            }
+            break;
+    }
+};
+
+client.MessageReceived += async message =>
+{
+    if (message.Content.Contains("button", StringComparison.OrdinalIgnoreCase))
+    {
+        try
+        {
+            var component = new ComponentBuilder()
+                .WithButton("Got it", "reminder-confirm", ButtonStyle.Success)
+                .WithButton("Snooze - 1 hour", "reminder-snooze-1h", ButtonStyle.Secondary)
+                .WithButton("Snooze - 24 hours", "reminder-snooze-24h", ButtonStyle.Secondary)
+                .Build();
+
+            await message.Channel.SendMessageAsync(
+                $"Some reminder {MentionUtils.MentionUser(message.Author.Id)}",
+                component: component);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+};
+
+await Task.Delay(-1);
---
+++ MihuBot/LocalTesting/Usings.cs
@@ -0,0 +1,15 @@
+﻿global using Discord;
+global using Discord.WebSocket;
+
+global using MihuBot;
+
+global using System;
+global using System.Diagnostics;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Net;
+global using System.Net.Http;
+global using System.Text;
+global using System.Threading;
+global using System.Threading.Tasks;
