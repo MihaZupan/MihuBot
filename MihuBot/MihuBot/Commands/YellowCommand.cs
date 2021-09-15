9448126fc3717a1afc4ea88fb1600d9ccbd01c8c
Miha Zupan <mihazupan.zupan1@gmail.com>
2021-09-15 04:19:59 +0200

    More yellow

--- MihuBot/MihuBot/Commands/YellowCommand.cs
+++ MihuBot/MihuBot/Commands/YellowCommand.cs
@@ -3,10 +3,24 @@
     public sealed class YellowCommand : CommandBase
     {
         public override string Command => "yellow";
+        public override string[] Aliases => new[] { "red" };
 
         public override async Task ExecuteAsync(CommandContext ctx)
         {
-            await ctx.Author.SendMessageAsync("https://cdn.discordapp.com/attachments/750706594412757094/887520872393478204/Color-yellow.jpg");
+            (string url, int number) =
+                ctx.Command == "yellow" ? ("https://cdn.discordapp.com/attachments/750706594412757094/887520872393478204/Color-yellow.jpg", 4) :
+                ctx.Command == "red" ? ("https://cdn.discordapp.com/attachments/750706594412757094/887522954739581009/blue.png", 1)
+                : (null, 0);
+
+            for (int i = 0; i < number; i++)
+            {
+                if (i != 0)
+                {
+                    await Task.Delay(1000);
+                }
+
+                await ctx.Author.SendMessageAsync(url);
+            }
         }
     }
 }
