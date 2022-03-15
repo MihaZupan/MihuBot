e7d52a1fc8e1abba2c2286f2656c6f5b736a2da8
Miha Zupan <mihazupan.zupan1@gmail.com>
2022-03-15 05:18:56 +0100

    Add civ handler

---
+++ MihuBot/MihuBot/NonCommandHandlers/Civ.cs
@@ -0,0 +1,32 @@
+﻿namespace MihuBot.NonCommandHandlers
+{
+    public class Civ : NonCommandHandler
+    {
+        private readonly Stopwatch _lastSentMessage = Stopwatch.StartNew();
+
+        public override Task HandleAsync(MessageContext ctx)
+        {
+            if (ctx.Guild.Id == Guilds.TheBoys && ctx.Content.Contains("civ", StringComparison.OrdinalIgnoreCase))
+            {
+                lock (_lastSentMessage)
+                {
+                    if (_lastSentMessage.Elapsed.TotalDays < 1)
+                    {
+                        return Task.CompletedTask;
+                    }
+
+                    _lastSentMessage.Restart();
+                }
+
+                return HandleAsyncCore();
+            }
+
+            return Task.CompletedTask;
+
+            async Task HandleAsyncCore()
+            {
+                await ctx.ReplyAsync("https://cdn.discordapp.com/emojis/818062423789142046.webp");
+            }
+        }
+    }
+}
