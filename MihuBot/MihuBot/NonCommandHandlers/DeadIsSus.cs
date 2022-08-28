27d032137ae21b01e951917361848dc6ec668a11
Miha Zupan <mihazupan.zupan1@gmail.com>
2022-08-29 01:06:10 +0200

    Add DeafIsSus toggle

--- MihuBot/MihuBot/NonCommandHandlers/DeadIsSus.cs
+++ MihuBot/MihuBot/NonCommandHandlers/DeadIsSus.cs
@@ -1,12 +1,16 @@
-﻿namespace MihuBot.NonCommandHandlers;
+﻿using MihuBot.Configuration;
+
+namespace MihuBot.NonCommandHandlers;
 
 public sealed class DeadIsSus : NonCommandHandler
 {
     private readonly DiscordSocketClient _discord;
+    private readonly IConfigurationService _configurationService;
 
-    public DeadIsSus(DiscordSocketClient discord)
+    public DeadIsSus(DiscordSocketClient discord, IConfigurationService configurationService)
     {
         _discord = discord ?? throw new ArgumentNullException(nameof(discord));
+        _configurationService = configurationService ?? throw new ArgumentNullException(nameof(configurationService));
     }
 
     public override Task HandleAsync(MessageContext ctx) => Task.CompletedTask;
@@ -23,7 +27,9 @@ public sealed class DeadIsSus : NonCommandHandler
                 after.VoiceChannel?.Id == ComfyZone &&
                 _discord.GetGuild(Guilds.TheBoys) is { } guild &&
                 guild.GetUser(user.Id) is { } guildUser &&
-                guild.GetVoiceChannel(DeafIsSus) is { } deafIsSus)
+                guild.GetVoiceChannel(DeafIsSus) is { } deafIsSus &&
+                _configurationService.TryGet(null, $"{nameof(DeafIsSus)}.Enabled", out var enabledString) &&
+                bool.TryParse(enabledString, out var enabled) && enabled)
             {
                 await guild.MoveAsync(guildUser, deafIsSus);
             }
