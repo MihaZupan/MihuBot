97fb2b88535de9e52f66cb908174973b7e3d1ea6
Miha Zupan <mihazupan.zupan1@gmail.com>
2022-10-13 21:35:25 +0200

    Add Sfae command

---
+++ MihuBot/MihuBot/Commands/SfaeCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace MihuBot.Commands
+{
+    public sealed class SfaeCommand : CommandBase
+    {
+        public override string Command => "sfae";
+
+        public override async Task ExecuteAsync(CommandContext ctx)
+        {
+            if (ctx.AuthorId != KnownUsers.Sfae)
+            {
+                string query = ctx.ArgumentString.Replace(' ', '+');
+
+                await Task.WhenAll(
+                    ctx.ReplyAsync($"https://letmegooglethat.com/?q={query}"),
+                    ctx.Channel.DeleteMessageAsync(ctx.Message));
+            }
+        }
+    }
+}
