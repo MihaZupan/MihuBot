fed2ecf13decbb9468ab2a8805de5f3e42713ed7
Miha Zupan <mihazupan.zupan1@gmail.com>
2024-11-05 22:22:53 +0100

    Allow randomizing imagine prompts

--- MihuBot/MihuBot/Commands/ImagineCommand.cs
+++ MihuBot/MihuBot/Commands/ImagineCommand.cs
@@ -1,6 +1,7 @@
 ﻿using Azure;
 using Azure.AI.OpenAI;
 using MihuBot.Configuration;
+using OpenAI.Chat;
 using OpenAI.Images;
 using System.Security.Cryptography;
 
@@ -16,14 +17,20 @@ public sealed class ImagineCommand : CommandBase
     private readonly Logger _logger;
     private readonly IConfigurationService _configurationService;
     private readonly AzureOpenAIClient _openAI;
+    private readonly AzureOpenAIClient _openAIChat;
 
     public ImagineCommand(Logger logger, IConfiguration configuration, IConfigurationService configurationService, IEnumerable<AzureOpenAIClient> openAI)
     {
         _logger = logger;
         _configurationService = configurationService;
+
         _openAI = new AzureOpenAIClient(
             new Uri("https://mihaz-m30zd4gd-eastus.openai.azure.com"),
             new AzureKeyCredential(configuration["AzureOpenAI:ImageKey"]));
+
+        _openAIChat = new AzureOpenAIClient(
+            new Uri("https://mihubotai8467177614.openai.azure.com"),
+            new AzureKeyCredential(configuration["AzureOpenAI:Key"]));
     }
 
     public override async Task ExecuteAsync(CommandContext ctx)
@@ -32,7 +39,9 @@ public sealed class ImagineCommand : CommandBase
 
         if (string.IsNullOrWhiteSpace(prompt))
         {
-            prompt = ctx.Author.GlobalName ?? ctx.Author.Username;
+            var promptPrompt = ChatMessage.CreateUserMessage("What's a cool prompt for Dall-e 3? Please reply with only the prompt, without quotes.");
+            ChatCompletion completion = (await _openAIChat.GetChatClient("gpt-4o").CompleteChatAsync(promptPrompt)).Value;
+            prompt = string.Concat(completion.Content.SelectMany(u => u.Text));
         }
 
         GeneratedImageSize size = GeneratedImageSize.W1024xH1024;
@@ -41,6 +50,11 @@ public sealed class ImagineCommand : CommandBase
         {
             prompt = prompt.Substring("large ".Length);
             size = GeneratedImageSize.W1792xH1024;
+        }
+        else if (prompt.StartsWith("tall ", StringComparison.OrdinalIgnoreCase))
+        {
+            prompt = prompt.Substring("tall ".Length);
+            size = GeneratedImageSize.W1024xH1792;
         }
 
         if (!_configurationService.TryGet(ctx.Guild.Id, "ChatGPT.ImageDeployment", out string deployment))
